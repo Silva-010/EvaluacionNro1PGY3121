c9c78d6637fbfaf451e884d17414d213d08cc1a9
silva-010 <franciscoignacio561@gmail.com>
2021-06-09 00:05:18 -0400

    commit

--- java.js
+++ java.js
@@ -1,5 +1,7 @@
+//se define la constante con la llave de la api
 const APP_ID = '4090239d69cdb3874de692fd18539299';
 
+//
 const fetchData = position => {
     const { latitude, longitude } = position.coords;
     fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${APP_ID}`)
